2a36ba5305f017b873fa6cf78d1f26e9f8ee16ce
Kenny F <kfrachelle123@gmail.com>
2018-02-13 08:43:27 +0800

    Update cherow.ts

--- src/cherow.ts
+++ src/cherow.ts
@@ -20,76 +20,34 @@ export interface Options {
     early?: boolean;
     impliedStrict?: boolean;
 }
+const parserClassCache: any  = {};
 
 function parse(source: string, context: Context, options: Options | void) {
 
     const comments: OnComment = [];
     let sourceFile: string = '';
-    let cherow: any = Parser;
 
     if (options != null) {
-        if (options.next) context |= Context.OptionsNext;
-        if (options.ranges) context |= Context.OptionsRanges;
-        if (options.raw) context |= Context.OptionsRaw;
-        if (options.loc) context |= Context.OptionsLoc;
-        if (options.ranges) context |= Context.OptionsRanges;
-        if (options.early) context |= Context.OptionsEarly;
-        if (options.impliedStrict) context |= Context.Strict;
         if (options.source) sourceFile = options.source;
-        if (options.comments) context |= Context.OptionsComments;
 
-        // TODO! Cache this, and make sure the extended class are extended
-        // once, and reused across parses.
         if (options.plugins) {
-            for (const plugin of options.plugins) {
-                cherow = plugin(Parser);
-            }
+            const key = options.plugins.join('/');
+            let cherow = parserClassCache[key];
+            if (!cherow) {
+                cherow = Parser;
+                for (const plugin of options.plugins) {
+                    cherow = plugin(cherow);
+                }
+                parserClassCache[key] = cherow;
+              }
+
+              return new cherow(source, comments, sourceFile).parseProgram(context, options);
         }
     }
 
-    const parser = new cherow(source, comments, sourceFile);
-
-    const node: ESTree.Program = {
-        type: 'Program',
-        sourceType: context & Context.Module ? 'module' : 'script',
-        body: context & Context.Module
-        ? parser.parseModuleItemList(context)
-        : parser.parseStatementList(context)
-    };
-
-    if (context & Context.OptionsRanges) {
-        node.start = 0;
-        node.end = source.length;
-    }
-
-    if (context & Context.OptionsLoc) {
-
-        node.loc = {
-            start: {
-                line: 1,
-                column: 0,
-            },
-            end: {
-                line: parser.line,
-                column: parser.column
-            }
-        };
-
-        if (sourceFile) {
-            (node.loc as any).source = sourceFile;
-        }
-    }
-
-    if (context & Context.OptionsEarly) {
-        node.earlyErrors = parser.earlyErors;
-    }
-
-    if (context & Context.OptionsComments) {
-        node.comments = parser.comments;
-    }
-
-    return node;
+    return new Parser(source, comments, sourceFile).parseProgram(context, options);
 }
+
  // https://tc39.github.io/ecma262/#sec-scripts
 
 export const parseScript = (source: string, options ?: Options) => {
