375db0837deb0700e2e51e6545e9ad0a4f7288ff
Kenny F <kfrachelle123@gmail.com>
2018-03-20 09:21:56 +0800

    Update parser.ts

--- src/parser.ts
+++ src/parser.ts
@@ -2404,7 +2404,7 @@ export class Parser {
 
             // VariableStatement[?Yield]
             case Token.VarKeyword:
-                return this.parseVariableStatement(context);
+                return this.parseVariableStatement(context | Context.AllowIn);
 
                 // BlockStatement[?Yield, ?Return]
             case Token.LeftBrace:
@@ -4711,6 +4711,8 @@ export class Parser {
             return this.parseArrowFunctionExpression(context & ~(Context.AllowAsync | Context.AllowYield), pos, isSequence ? (expr as any).expressions : [expr], params);
         }
 
+        this.flags &= ~Flags.HasYield;
+
         return expr;
     }
 
@@ -5071,7 +5073,7 @@ export class Parser {
         let id: ESTree.Identifier | undefined | null = null;
 
         // Unset masks Object / Class Method, and disallow derived class constructors in this context
-        context &= ~(Context.Method | Context.AllowSuperProperty | Context.AllowYield);
+        context &= ~(Context.Method | | Context.AnnexB | Context.AllowSuperProperty | Context.AllowYield);
 
         const prevContext = context;
 
