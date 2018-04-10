761d9c4ae39383d12c65f7de4372933ce27ac147
kflash <kfrachelle123@gmail.com>
2018-04-10 20:49:04 +0800

    removed duplicate option bitmask

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -18,7 +18,11 @@ import {
  * @param sourceFile Optional source file info to be attached in every node
  * @param delegate  Optional callback function to be invoked for each syntax node (as the node is constructed)
  */
-export function createParserObj(source: string, sourceFile: string | void, delegate: Delegate | void): Parser {
+export function createParser(
+    source: string,
+    sourceFile: string | void,
+    delegate: Delegate | void
+): Parser {
     return {
         // The source code to parse
         source: source,
@@ -73,28 +77,40 @@ export function parse(source: string, options: Options | void, context: Context)
     let delegate;
 
     if (!!options) {
+        // The flag to enable stage 3 support (ESNext)
         if (options.next) context |= Context.OptionsNext;
+        // The flag to enable React JSX parsing
         if (options.jsx) context |= Context.OptionsJSX;
+        // The flag to enable start and end offsets to each node
         if (options.ranges) context |= Context.OptionsRanges;
+        // The flag to enable line/column location information to each node
         if (options.loc) context |= Context.OptionsLoc;
+        // The flag to attach raw property to each literal node
         if (options.raw) context |= Context.OptionsRaw;
-        if (options.delegate) context |= Context.OptionsDelegate;
+        // The flag to allow return in the global scope
         if (options.globalReturn) context |= Context.OptionsGlobalReturn;
+        // The flag to allow 'await' in the global scope
         if (options.globalAwait) context |= Context.OptionsGlobalAwait;
+        // The flag to allow to skip shebang - '#'
         if (options.skipShebang) context |= Context.OptionsShebang;
+        // Attach raw property to each identifier node
         if (options.rawIdentifier) context |= Context.OptionsRawidentifiers;
+        // Enable tolerant mode
         if (options.tolerant) context |= Context.OptionsTolerant;
+        // Set to true to record the source file in every node's loc object when the loc option is set.
         if (!!options.source) sourceFile = options.source;
+        // Create a top-level comments array containing all comments
         if (!!options.comments) context |= Context.OptionsComments;
-
+        // The flag to enable implied strict mode
         if (options.impliedStrict) context |= Context.OptionsImpliedStrict
-
-        delegate = (typeof options.delegate === 'function') ? options.delegate : undefined;
-
-        if (delegate) context |= Context.OptionsDelegate;
+        // Accepts a callback function to be invoked for each syntax node (as the node is constructed)
+        if (typeof options.delegate === 'function') {
+            context |= Context.OptionsDelegate;
+            delegate = options.delegate;
+        }
     }
 
-    const parser = createParserObj(source, sourceFile, delegate);
+    const parser = createParser(source, sourceFile, delegate);
 
     const body = context & Context.Module ? parseModuleItemList(parser, context) : parseStatementList(parser, context);
 
@@ -122,18 +138,13 @@ export function parse(source: string, options: Options | void, context: Context)
             }
         };
 
-        if (sourceFile) {
-            node.loc.source = sourceFile;
-        }
+        if (sourceFile) node.loc.source = sourceFile;
+
     }
 
-    if (context & Context.OptionsComments) {
-        node.comments = parser.comments;
-    }
+    if (context & Context.OptionsComments) node.comments = parser.comments;
 
-    if (context & Context.OptionsTolerant) {
-        node.errors = parser.errors;
-    }
+    if (context & Context.OptionsTolerant) node.errors = parser.errors;
 
     return node;
 }
