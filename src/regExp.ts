57947fa673db88f02537cb6ee619f5e950656784
kflash <kfrachelle123@gmail.com>
2018-04-15 12:32:13 +0800

    removed unused code

--- src/regExp.ts
+++ src/regExp.ts
@@ -4,18 +4,15 @@ import { isValidIdentifierStart, isValidIdentifierPart } from "./unicode"
 import { Errors, ErrorMessages } from './errors';
 import { createParser } from './parser/parser';
 import { Parser } from './types';
-
 /**
  * Public regular expression validator
  * 
  * @param source regexp pattern to validate
  * @param isUnicode true if unicode.
  */
-
 export function validateRegExp(source: string, isUnicode: boolean): boolean {
     const parser = createParser(source, undefined, undefined);
-    validatePattern(parser, isUnicode);
-    return true;
+    verifyRegExpPattern(parser, context, 0, source.length);
 }
 
 /**
@@ -24,7 +21,41 @@ export function validateRegExp(source: string, isUnicode: boolean): boolean {
  * @param Parser Parser instance
  * @param isUnicode true if unicode.
  */
-export function validatePattern(parser: Parser, isUnicode: boolean): boolean {
+export function verifyRegExpPattern(
+    parser: Parser,
+    isUnicode: boolean,
+    start: number,
+    end: number
+) {
+    const groupNames: string[] = [];
+    const referenceNames: string[] = [];
+    if (isUnicode) context |= ValidatorState.NamedGroups;
+    parsePattern(parser, context, start, end, groupNames, referenceNames);
+    if (!(context & ValidatorState.Unicode) && groupNames.length > 0) {
+        parsePattern(parser, context & ~ValidatorState.Unicode, /* empty */ [], /* empty*/ [])
+    }
+}
+
+/**
+ * Parse regexp pattern
+ * 
+ * @param Parser Parser instance
+ * @param context Validator context
+ * @param start Start of regexp pattern
+ * @param end End of regexp pattern
+ * @param groupnames Collection of groupnames
+ * @param referenceNames Collection of reference names
+ */
+export function parsePattern(
+    parser: Parser,
+    context: ValidatorState,
+    start: number,
+    end: number,
+    groupNames: string[],
+    referenceNames: string[]
+) {
     return false;
-}    
- 
+    // while (start !== end) {
+    // To be added
+    // }
+}
