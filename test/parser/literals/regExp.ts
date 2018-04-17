0c1f8ad27e030dd8287e0e5a8903446e1ef4fabd
kflash <kfrachelle123@gmail.com>
2018-04-17 13:16:44 +0800

    landing

--- test/parser/literals/regExp.ts
+++ test/parser/literals/regExp.ts
@@ -2,28 +2,9 @@ import { pass, fail } from '../../test-utils';
 import { Context } from '../../../src/utilities';
 import * as t from 'assert';
 import { parse } from '../../../src/parser/parser';
-import { validateRegularExpression } from '../../../src/cherow';
 
 describe('Literals - Reggular expression', () => {
 
-/*
-    // This tests will not work before I push the validator code
-
-    describe('Cherow.validateRegularExpression()', () => {
-
-        const invalidSyntax = [
-            "/(?=/"
-        ]
-
-        for (const arg of invalidSyntax) {
-            it(`${arg}`, () => {
-                t.throws(() => {
-                    validateRegularExpression(`${arg}`, false)
-                })
-            })
-        }
-    });
-*/
     describe('Failure', () => {
 
         const invalidSyntax = [
