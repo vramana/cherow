7405cd70f44b5c9d3f7056ec9286ccf85bcc5550
kflash <kfrachelle123@gmail.com>
2018-04-24 09:47:21 +0800

    added a few more tests
    - J. K Thomas

--- test/parser/miscellaneous/chinese.ts
+++ test/parser/miscellaneous/chinese.ts
@@ -3,31 +3,29 @@ import { Context } from '../../../src/utilities';
 
 describe('Miscellaneous - Chinese', () => {
 
-    describe('Failure', () => {});
+  describe('Failure', () => {});
 
-    describe('Pass', () => {
+  describe('Pass', () => {
 
-        pass(`class 𢭃 { /* 𢭃 */ }`, Context.Empty, {
-            source: 'class 𢭃 { /* 𢭃 */ }',
-            expected: {
-                  body: [
-                    {
-                      body: {
-                        body: [],
-                        type: 'ClassBody'
-                      },
-                      id: {
-                        name: '𢭃',
-                        type: 'Identifier',
-                      },
-                      superClass: null,
-                      type: 'ClassDeclaration',
-                    },
-                  ],
-                  sourceType: 'script',
-                  type: 'Program',
-                }
-        });
+      pass(`class 𢭃 { /* 𢭃 */ }`, Context.Empty, {
+          source: 'class 𢭃 { /* 𢭃 */ }',
+          expected: {
+              body: [{
+                  body: {
+                      body: [],
+                      type: 'ClassBody'
+                  },
+                  id: {
+                      name: '𢭃',
+                      type: 'Identifier',
+                  },
+                  superClass: null,
+                  type: 'ClassDeclaration',
+              }, ],
+              sourceType: 'script',
+              type: 'Program',
+          }
+      });
 
-    });
+  });
 });
--- test/parser/miscellaneous/global-code.ts
+++ test/parser/miscellaneous/global-code.ts
@@ -2,39 +2,37 @@ import { pass, fail } from '../../test-utils';
 import { Context } from '../../../src/utilities';
 import * as t from 'assert';
 import { parse } from '../../../src/parser/parser';
-
 describe('Expressions - Global code', () => {
 
-    describe('Failure', () => {
+  describe('Failure', () => {
 
-// super in global scope
-fail(`() => { super(); };`, Context.Empty, {
-    source: '() => { super.foo; };',
-});
+      // super in global scope
+      fail(`() => { super(); };`, Context.Empty, {
+          source: '() => { super.foo; };',
+      });
 
-fail(`() => { super(); };`, Context.Empty, {
-  source: '() => { super.foo; };',
-});
+      fail(`() => { super(); };`, Context.Empty, {
+          source: '() => { super.foo; };',
+      });
 
-fail(`super.property;`, Context.Empty, {
-  source: 'super.property;',
-});
+      fail(`super.property;`, Context.Empty, {
+          source: 'super.property;',
+      });
 
-fail(`return;`, Context.Empty, {
-    source: 'return;',
+      fail(`return;`, Context.Empty, {
+          source: 'return;',
+      });
+
+      fail(`export default null;`, Context.Empty, {
+          source: 'export default null;',
+      });
+
+      //fail(`export default null;`, Context.Strict | Context.Module, {
+      //    source: 'export default null;',
+      //});
+
+      //fail(`() => { new.target; };`, Context.Empty, {
+      //  source: '() => { new.target; };',
+      //});
   });
-
-fail(`export default null;`, Context.Empty, {
-    source: 'export default null;',
-  });
-
-  //fail(`export default null;`, Context.Strict | Context.Module, {
-//    source: 'export default null;',
-  //});
-
-//fail(`() => { new.target; };`, Context.Empty, {
-  //  source: '() => { new.target; };',
-  //});
-
-    });
 });
