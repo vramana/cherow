eef97744bfa64ad574b8576a9c77ab2c454be618
kflash <kfrachelle123@gmail.com>
2018-04-24 12:32:36 +0800

    added complex block scope tests
    added complex block scope tests. Works as a guard when we start to implement scoping
    
    J. K. Thomas (drop-by-dev)

--- test/parser/miscellaneous/scoping.ts
+++ test/parser/miscellaneous/scoping.ts
@@ -17,15 +17,15 @@ describe('Miscellaneous - Scoping', () => {
             // Complex parameter shouldn't be shadowed
             `(function foo(x = 0) { var x; { function x() {}} })(1);`,
             // Nested complex parameter shouldn't be shadowed
-            `(function shadowingVarNestedParameterDoesntBind([[x]]) { var x;{ function x() {} }  })([[1]]);`,
+            `(function foo([[x]]) { var x;{ function x() {} }  })([[1]]);`,
             // Rest parameter shouldn't be shadowed
-            `(function shadowingVarRestParameterDoesntBind(...x) { var x; {  function x() {}  } })(1);`,
+            `(function foo(...x) { var x; {  function x() {}  } })(1);`,
             // Don't shadow complex rest parameter
-            `(function shadowingVarComplexRestParameterDoesntBind(...[x]) { var x; { function x() {} } })(1);`,
+            `(function foo(...[x]) { var x; { function x() {} } })(1);`,
             // Hoisting is not affected by other simple parameters
-            `(function irrelevantParameterBinds(y, z) {{function x() {}} })(1);`,
+            `(function foo(y, z) {{function x() {}} })(1);`,
             // Hoisting is not affected by other complex parameters
-            ` (function irrelevantComplexParameterBinds([y] = [], z) {{function x() {} } })();`,
+            ` (function foo([y] = [], z) {{function x() {} } })();`,
             // Should allow shadowing function names
             `{(function foo() { { function foo() { return 0; } } })();}`,
             `{(function foo(...r) { { function foo() { return 0; } } })(); }`,
@@ -33,9 +33,15 @@ describe('Miscellaneous - Scoping', () => {
             `(function foo() { var y = 1; (function bar(x = y) { { function y() {} } })();  })();`,
             `(function foo() { { let f = 2; { let y = 3; function f() { y = 2; } f(); } }})();`,
             `(function foo() { { function f() { return 4; } { function f() { return 5; } } }})()`,
-            '(function foo(a = 0) { { let y = 3; function f(b = 0) { y = 2; } f(); } })();'
+            '(function foo(a = 0) { { let y = 3; function f(b = 0) { y = 2; } f(); } })();',
+            '(function conditional() {  if (true) { function f() { return 1; } } else {  function f() { return 2; }} if (false) { function g() { return 1; }}  L: {break L;function f() { return 3; } }})();',
+            '(function foo() {function outer() { return f; } { f = 1; function f () {} f = ""; } })();',
+            '(function foo(x) { {  function x() {} } })(1);',
+            '(function foo([[x]]) { { function x() {}}})([[1]]);',
+             // rest parameter shouldn't be shadowed
+             '(function shadowingRestParameterDoesntBind(...x) { {  function x() {} } })(1);'
         ];
-
+        
         for (const arg of validSyntax) {
             it(`${arg}`, () => {
                 t.doesNotThrow(() => {
