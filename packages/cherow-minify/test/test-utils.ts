7a2d70f0a5834d6f9a38998e80781842c8837b6c
Fred Kleuver <fred@avurad.nl>
2018-05-08 01:00:59 +0200

    tests(*): fix test runner

--- packages/cherow-minify/test/test-utils.ts
+++ packages/cherow-minify/test/test-utils.ts
@@ -1,27 +1,27 @@
 import * as assert from 'clean-assert';
+import * as ESTree from '../../cherow/src/estree';
+import { Context } from '../../cherow/src/utilities';
 import * as t from 'assert';
-import { parse } from '../src/parser/parser';
-import { Context } from '../src/utilities';
-import * as ESTree from '../src/estree';
+import { parse } from '../../cherow/src/parser/parser';
 
 export interface Opts {
-    source: string;
-    expected?: any;
-    line ?: number;
-    column ?: number;
+  source: string;
+  expected?: any;
+  line?: number;
+  column?: number;
 }
 
 export const pass = (name: string, context: Context, opts: Opts) => {
-    it(name, () => {
-        const parser = parse(opts.source, undefined, context);
-        assert.match(parser, opts.expected);
-    });
+  it(name, () => {
+    const parser = parse(opts.source, undefined, context);
+    assert.match(parser, opts.expected);
+  });
 };
 
 export const fail = (name: string, context: Context, opts: Opts) => {
-    it(name, () => {
-        assert.throws(SyntaxError, () => {
-            parse(opts.source, undefined, context);
-        });
+  it(name, () => {
+    assert.throws(SyntaxError, () => {
+      parse(opts.source, undefined, context);
     });
+  });
 };
--- packages/cherow-ts/test/test-utils.ts
+++ packages/cherow-ts/test/test-utils.ts
@@ -5,23 +5,23 @@ import * as t from 'assert';
 import { parse } from '../../cherow/src/parser/parser';
 
 export interface Opts {
-    source: string;
-    expected?: any;
-    line ?: number;
-    column ?: number;
+  source: string;
+  expected?: any;
+  line?: number;
+  column?: number;
 }
 
 export const pass = (name: string, context: Context, opts: Opts) => {
-    it(name, () => {
-        const parser = parse(opts.source, undefined, context);
-        assert.match(parser, opts.expected);
-    });
+  it(name, () => {
+    const parser = parse(opts.source, undefined, context);
+    assert.match(parser, opts.expected);
+  });
 };
 
 export const fail = (name: string, context: Context, opts: Opts) => {
-    it(name, () => {
-        assert.throws(SyntaxError, () => {
-            parse(opts.source, undefined, context);
-        });
+  it(name, () => {
+    assert.throws(SyntaxError, () => {
+      parse(opts.source, undefined, context);
     });
+  });
 };
